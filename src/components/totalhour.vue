2218d678abdc4afd34546a763e7b23c74f12646d
Meshi9 <rivka@tik-tak.co.il>
2021-06-27 14:58:53 +0300

    created component shaed test

---
+++ src/components/totalhour.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+      <input type="checkbox" value="true" name="thisButton" @:check="inputchange" class="color-red"/>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Schedule',
+  data:{
+      colorBox,
+  },
+  method:{
+   inputchange:function() {
+
+   }
+  }
+}
+</script>
